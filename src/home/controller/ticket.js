983e014cc70d604fbbe5d3676d7e2d5341836c86
leiquan <leiquan@baidu.com>
2016-04-27 14:57:45 +0800

    up

--- src/home/controller/ticket.js
+++ src/home/controller/ticket.js
@@ -11,6 +11,7 @@ export default class extends think.controller.rest {
    */
   init(http){
     super.init(http);
+    this._method = "_method"; //指定请求类型从 GET 参数 _method 里获取,用于处理不支持 delete 操作的客户端
   }
   /**
    * before magic method
